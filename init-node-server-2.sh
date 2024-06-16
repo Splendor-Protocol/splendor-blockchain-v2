f035d924a62fce5902ff337ee329a79b03697a7f
diego1996 <diego.asencio.cuellar@unillanos.edu.co>
2024-06-16 15:53:14 -0500

    fix: config por 2-nodes

--- init-node-server-2.sh
+++ init-node-server-2.sh
@@ -2,6 +2,9 @@
 
 PASSWORD="Pl3n1TuD"
 
+plenitud-template-node key insert --base-path /data/node02 --chain ./customSpecRaw.json --scheme Sr25519 --suri "electric pass pioneer magnet reward upon gloom deal pigeon cluster snake width" --password $PASSWORD --key-type aura
+plenitud-template-node key insert --base-path /data/node02 --chain ./customSpecRaw.json --scheme Ed25519 --suri "capital mixture elephant message current genre expire come absent monkey frog strike" --password $PASSWORD --key-type gran
+
 plenitud-template-node --base-path /data/node02 \
   --chain ./customSpecRaw.json \
   --port 30334 \
